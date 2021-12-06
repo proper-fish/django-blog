ec825ff409b084e8f520c35c57fca305c8726ac2
proper-fish <aliona.kozushkina@gmail.com>
2021-12-06 20:41:20 +0300

    the app is run in Docker w/ a PostgresQL database

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.8-alpine
+
+ENV PYTHONBUFFERED 1
+
+COPY ./requirements.txt /requirements.txt
+
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps gcc libc-dev linux-headers postgresql-dev
+RUN pip install -r requirements.txt
+RUN apk del .tmp-build-deps
+
+RUN mkdir /django-blog
+WORKDIR /django-blog
+COPY . /django-blog
+
+#db username
+RUN adduser -D user
+USER user
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- mysite/settings.py
+++ mysite/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
