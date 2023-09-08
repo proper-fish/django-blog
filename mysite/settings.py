b85d2cf9a645f128e8f5d2c09e8585388a4a327f
proper-fish <aliona.kozushkina@gmail.com>
2023-09-08 15:02:12 +0400

    changed secret key setting + minor template fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,8 +28,7 @@ if os.path.isfile(dotenv_file):
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
